5e5037f944b8ed70d9c6d49e7ef30c43116af69a
Luan Thieu Nguyen <luannguyen@bbv.ch>
2024-10-02 18:40:35 +0700

    feat: number label

--- src/app/components/money-display/money-display.component.ts
+++ src/app/components/money-display/money-display.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-money-display',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './money-display.component.html',
   styleUrl: './money-display.component.scss',
 })
 export class MoneyDisplayComponent {
   @Input() value = 0;
+  @Input() imgSrc = '';
 }
